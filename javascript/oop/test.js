78a6ab35666a1f2dd9094a32187aa2ca4f9ed4d5
Calvin-T <calvin+1@sliame.com>
2020-05-05 19:17:20 +0200

    training

--- javascript/oop/test.js
+++ javascript/oop/test.js
@@ -24,14 +24,18 @@
 // global.setTimeout(callbacks, timeToWait)
 
 
-for (let i = 0; i < 130; i++) {
-    if (i % 15 == 0) {
-        console.log("fizzbuzz")
-    } else if (i % 3 == 0) {
-        console.log("fizz")
-    } else if (i % 5 == 0) {
-        console.log("buzz")
-    } else {
-        console.log(i)
-    }
-}
+// for (let i = 0; i < 130; i++) {
+//     if (i % 15 == 0) {
+//         console.log("fizzbuzz")
+//     } else if (i % 3 == 0) {
+//         console.log("fizz")
+//     } else if (i % 5 == 0) {
+//         console.log("buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+let dave = 7
+
+console.log(dave)
